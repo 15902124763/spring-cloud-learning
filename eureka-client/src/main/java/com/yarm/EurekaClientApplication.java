aaa1ed8ae913f730eb13f8c8dd10df526a10e804
yarm <15902124763@163.com>
2019-10-30 16:16:05 +0800

    #add eureka

---
+++ eureka-client/src/main/java/com/yarm/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.yarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created with IDEA
+ * author:Yarm.Yang
+ * Date:2019/10/30
+ * Time:14:52
+ * Des:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class,args);
+    }
+}
---
+++ eureka-server/src/main/java/com/yarm/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.yarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IDEA
+ * author:Yarm.Yang
+ * Date:2019/10/30
+ * Time:11:22
+ * Des:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
